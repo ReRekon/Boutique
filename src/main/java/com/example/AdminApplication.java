84e159b222a801e9c9d71d47d293f2a99a3002c1
Zuojhao <1585661351@qq.com>
2019-08-06 21:38:54 +0800

    Zuojiahao的提交

--- src/main/java/com/example/AdminApplication.java
+++ src/main/java/com/example/AdminApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-@MapperScan("com.example.mapper")
+@MapperScan("com.study.admin.mapper")
 public class AdminApplication {
 
     public static void main(String[] args) {
