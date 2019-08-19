06416706f32350ec371d17d2c881ee7126afbb3c
HRK <819071732@qq.com>
2019-08-19 11:11:34 +0800

    备份

--- Boutique/src/main/java/com/example/DemoApplication.java
+++ Boutique/src/main/java/com/example/DemoApplication.java
@@ -3,12 +3,13 @@ package com.example;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @MapperScan("com.example.mapper")
 @SpringBootApplication
 @EnableTransactionManagement
-public class DemoApplication {
+public class DemoApplication extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
         SpringApplication.run(DemoApplication.class, args);
